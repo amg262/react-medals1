539635b4beed03fc3263488d5b76be29b16229b9
Andrew <andrewmgunn26@gmail.com>
2022-09-12 10:04:26 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Country from './components/Country';
+import './App.css';
+
+class App extends Component {
+  state = {  }
+  render() { 
+    return (
+      <Country />
+    );
+  }
+}
+ 
+export default App;
